f56d27d3c51cb0d6ecff13c23fd02b775a27ff6b
JnnnLe <jnnnle.sf@gmail.com>
2018-08-13 15:16:56 -0700

    Fibinacci sequence by caching and recursion

--- LinkedLists/Recursion/Fib.js
+++ LinkedLists/Recursion/Fib.js
@@ -2,4 +2,12 @@ const fibinacci = (n) => {
   if (n == 0) return 0;
   if (n == 1 || n == 1) return 1;
   return fibinacci(n - 1) + fibinacci(n - 2);
+};
+
+
+// Implementing Fibinacci sequence, using recursion and caching.
+const memoization_fib = (n, memo = []) => {
+  memo = [0, 1, 1];
+  if (memo[n] != null) return memo[n];
+  return memo[n] = memoization_fib(n - 1) + memoization_fib(n - 2);
 }
