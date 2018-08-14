11169617b70ce168cea981287b1c68a96a7cad2e
JnnnLe <jnnnle.sf@gmail.com>
2018-08-13 18:20:38 -0700

    Rewrote Node class and implemented insert for bst

--- Trees/BST/BST.js
+++ Trees/BST/BST.js
@@ -1,7 +1,39 @@
 import Node from '../Node';
 
-class BinarySearchTree {
+class bst {
   constructor() {
-    
+    this.root = null;
   }
+
+  insert(value) {
+    if (this.root == null) {
+      this.root = new Node(value);
+    } else {
+      this.insertNode(this.root, value);
+    }
+  };
+
+  // helper fn for inserting new Node into tree
+  insertNode(node, value) {
+    // check for value against value of node
+    if (node.value > value) {
+      // if the node is not yet defined, leaf node
+      if (node.left == null) {
+        // create new Node and insert
+        node.left = new Node(value);
+      } else {
+        // else recursively call down subtree
+        this.insertNode(node.left, value);
+      }
+    
+    } else {
+      if (node.right == null) {
+        node.right = new Node(value);
+      } else {
+        this.insertNode(node.right, value);
+      }
+    }
+  };
+
+// end of class
 }
