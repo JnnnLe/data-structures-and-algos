31efbb90441c20e8de9c7165812bec8a7513f4fc
JnnnLe <jnnnle.sf@gmail.com>
2018-08-25 10:28:38 -0700

    Implemented Sort, Bubble, Bubble, BubbleSort

--- Sort/1-BubbleSort.js
+++ Sort/1-BubbleSort.js
@@ -1,6 +1,36 @@
-const bubbleSort = (arr) => {
-  if (arr.length < 2) return arr;
+const bubbleSort = (originalArray) => {
+    // Flag that holds info about whether the swap has occur or not.
+    let swapped = false;
+    // Clone original array to prevent its modification.
+    const array = [...originalArray];
 
-};
+    for (let i = 1; i < array.length; i++) {
+      swapped = false;
+
+      // Call visiting callback.
+
+      for (let j = 0; j < array.length - i; j++) {
+        // Call visiting callback.
+
+        // Swap elements if they are in wrong order.
+        if (array[j + 1] < array[j]) {
+          const tmp = array[j + 1];
+          array[j + 1] = array[j];
+          array[j] = tmp;
+
+          // Register the swap.
+          swapped = true;
+        }
+      }
+
+      // If there were no swaps then array is already sorted and there is
+      // no need to proceed.
+      if (!swapped) {
+        return array;
+      }
+    }
+
+    return array;
+}
 
 bubbleSort([10,6,7,2,3,19,1,8,9]);
