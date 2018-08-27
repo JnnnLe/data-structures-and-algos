ee1abb70850001333a45b464fdaf73a6cc629124
JnnnLe <jnnnle.sf@gmail.com>
2018-08-27 14:27:28 -0700

    Implemented another way to write bubbleSort, bubble

--- Sort/1-BubbleSort.js
+++ Sort/1-BubbleSort.js
@@ -31,3 +31,20 @@ const bubbleSort = (originalArray) => {
 }
 
 bubbleSort([10,6,7,2,3,19,1,8,9]);
+
+
+// less code, same effect
+const bubble = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        let temp = arr[j+1];
+        arr[j+1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  };
+  return arr;
+};
+
+bubble([10,8,2,3,5,6,7,2,3,16,96,3])
