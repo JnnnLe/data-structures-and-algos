38128e7f809cf76b170ca4b4181b413c5b389ab6
JnnnLe <jnnnle.sf@gmail.com>
2018-08-27 17:13:04 -0700

    Implemented Insertion sort

--- Sort/3-Insertion/InsertionSort.js
+++ Sort/3-Insertion/InsertionSort.js
@@ -1,3 +1,19 @@
-const insertionSort = () => {
-  
-}
+const insertionSort = (arr) => {
+  let temp;
+
+  for (let i = 1; i < arr.length; i++) {
+    let j = i;
+    temp = arr[i];
+
+    while (j > 0 && [temp] < arr[j-1]) {
+      arr[j] = arr[j-1];
+      j--;
+    }  
+
+    arr[j] = temp;
+  }
+
+  return arr;
+};
+
+insertionSort([5,4,3,2,1]);
